f395dc3acf3e1e04fd096bfc40885c5d9a822238
Furkan Çelik <furkancelikbm@hotmail.com>
2024-07-10 21:56:43 +0300

    an mobile application that can only be used by users with a certain university email adress.In a way we can say answer-question mobile app

--- README.md
+++
@@ -1,8 +0,0 @@
-an mobile application that can only be used by users with a certain university email adress.In a way we can say answer-question mobile app
-
-
-![login](https://github.com/furkancelikbm/Social-Therapy-Chatting-App/assets/168982729/69e2650f-634d-4fd8-9651-aa4d94b6b0ea)
-![interface](https://github.com/furkancelikbm/Social-Therapy-Chatting-App/assets/168982729/16719691-71a5-4638-9cf2-e454f9257c7d)
-![chat](https://github.com/furkancelikbm/Social-Therapy-Chatting-App/assets/168982729/5cca9f9b-1920-44a9-ac52-27116d61ac09)
-![dashboard](https://github.com/furkancelikbm/Social-Therapy-Chatting-App/assets/168982729/ecb7d7a0-fd22-42e4-a912-24ba5b7b5afe)
-![chatting](https://github.com/furkancelikbm/Social-Therapy-Chatting-App/assets/168982729/26af0f47-36b0-44c4-8ca1-6e079780746d)
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,31 +1,36 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+
+
 
 
 }
 
+
+
+
+
+
 android {
-    namespace = "com.example.answerandquestion"
-    compileSdk = 31
-
-
-    buildFeatures {
-        viewBinding
-    }
+    namespace = "com.example.myprojectnew"
+    compileSdk = 34
 
 
 
     defaultConfig {
-        applicationId = "com.example.answerandquestion"
+        applicationId = "com.example.myprojectnew"
         minSdk = 21
-        targetSdk = 31
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
+    buildFeatures {
+        viewBinding =true
+    }
 
     buildTypes {
         release {
@@ -45,53 +50,54 @@ android {
     }
 }
 
+
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.github.mukeshsolanki:android-otpview-pinview:2.1.2")
-    implementation ("com.github.User:Repo:Version")
-    // Import the BoM for the Firebase platform
-    implementation ("com.google.firebase:firebase-bom:32.5.0")
 
-    // Declare the dependencies for the desired Firebase products without specifying versions
-    // For example, declare the dependencies for Firebase Authentication and Cloud Firestore
-    implementation ("com.google.firebase:firebase-auth")
-    implementation ("com.google.firebase:firebase-firestore:24.9.1")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
     implementation ("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-auth")
     implementation ("com.google.firebase:firebase-database")
-    implementation ("com.google.firebase:firebase-storage")
     implementation ("com.google.firebase:firebase-messaging")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
-    implementation ("com.github.bumptech.glide:glide:4.11.0")
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation ("com.github.pgreze:android-reactions:1.6")
-    implementation ("com.devlomi:circularstatusview:1.0.1")
+    implementation ("com.github.3llomi:CircularStatusView:V1.0.3")
     implementation ("com.github.OMARIHAMZA:StoryView:1.0.2-alpha")
     implementation ("com.github.sharish:ShimmerRecyclerView:v1.3")
-    implementation ("com.github.mukeshsolanki.android-otpview-pinview:otpview-compose:3.1.0")
-    implementation ("com.github.mukeshsolanki.android-otpview-pinview:otpview:3.1.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-auth:22.3.0")
+    implementation ("com.google.firebase:firebase-auth:latest_version")
+
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("com.github.bumptech.glide:okhttp3-integration:4.7.1")
+    implementation ("com.firebaseui:firebase-ui-storage:7.2.0")
 
 
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("com.google.firebase:firebase-storage:20.3.0")
+// Also add the dependency for the Google Play services library and specify its version
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
 
-
-
-
-
-
-
-
-
-
-
-
-
+    // Add the dependencies for the App Check libraries
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-appcheck-playintegrity")
 
 
 
 }
+
